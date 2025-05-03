bb0295ce4e65fddd7180b7a7844a1c96891f5d6f
lnunes9393 <143144216+lnunes9393@users.noreply.github.com>
2025-05-03 18:54:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
 @app.route('/')
-def home(): return 'Hello World'
+def home():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  # Porta fornecida pelo Render
+    app.run(host='0.0.0.0', port=port)
